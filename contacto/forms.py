3018e26f72f6bae80425c93f2309e0816034d01c
gjeremy29 <gjeremy0229@outlook.es.com>
2023-06-09 16:23:25 -0500

    contact2

--- contacto/forms.py
+++ contacto/forms.py
@@ -1,10 +1,9 @@
 from django import forms
-from .models import Contact
 
-class ContactForm(forms.ModelForm):
-
-    class Meta:
-        model = Contact
-        fields = ['name', 'email', 'contact_type', 'ruc', 'direccion', 'message', 'subscription']
-
-
+class ContactForm(forms.Form):
+    nombres = forms.CharField(max_length=100, required=True)
+    correo = forms.EmailField(required=True)
+    ruc = forms.CharField(max_length=13, required=True)
+    direccion = forms.CharField(max_length=50, required=True)
+    mensaje = forms.CharField(widget=forms.Textarea, required=True)
+    aceptar_condiciones = forms.BooleanField(required=True, label='Cuenta con tienda física o en línea')
--- contacto/views.py
+++ contacto/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ContactForm
 # Create your views here.
 
 
 def Contact(request):
     contact_form = ContactForm()
+
+    if request.method=="POST":
+        contact_form = ContactForm(data=request.POST)
+        if contact_form.is_valid():
+            nombres=request.POST.get("nombres")
+            correo=request.POST.get("correo")
+            ruc=request.POST.get("Ruc")
+            direccion= request.POST.get("Direccion")
+            mensaje=request.POST.get("Mensaje")
+            aceptar_condiciones= request.POST.get(label="aceptar_condiciones")
+        
+            return redirect("/contacto/?valido")
+        
+
     return render(request, "contact/contact.html", {'form':contact_form})
