f8c6f62ed9d6476ee62e75b9485b054c859b5b2a
gjeremy29 <gjeremy0229@outlook.es.com>
2023-06-16 16:27:28 -0500

    style_end

--- contacto/forms.py
+++ contacto/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    nombres = forms.CharField(max_length=100, required=True)
-    correo = forms.EmailField(required=True)
-    celular = forms.CharField(max_length=13, required=True)
-    ruc = forms.CharField(max_length=13, required=True)
-    direccion = forms.CharField(max_length=50, required=True, label='Dirección')
-    mensaje = forms.CharField(widget=forms.Textarea, required=True)
+    nombres = forms.CharField(max_length=100, required=True,  widget=forms.TextInput(attrs={'class': 'form-control'}))
+    correo = forms.EmailField(required=True,  widget=forms.TextInput(attrs={'class': 'form-control'}))
+    celular = forms.CharField(max_length=13, required=True,  widget=forms.TextInput(attrs={'class': 'form-control'}))
+    ruc = forms.CharField(max_length=13, required=True,  widget=forms.TextInput(attrs={'class': 'form-control'}))
+    direccion = forms.CharField(max_length=50, required=True, label='Dirección',  widget=forms.TextInput(attrs={'class': 'form-control'}))
+    mensaje = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}), required=True)
     aceptar_condiciones = forms.BooleanField(required=True, label='Cuenta con tienda física o en línea')
